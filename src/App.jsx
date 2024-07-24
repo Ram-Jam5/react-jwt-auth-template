1ba3c2b21e86b5aa29915c329b8a0267e073d53c
Ram-Jam5 <edgeillusive@gmail.com>
2024-07-24 14:21:02 -0500

    create sign-up functionality and form

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar/NavBar';
 import Landing from './components/Landing/Landing';
 import Dashboard from './components/Dashboard/Dashboard';
+import SignupForm from './components/SignupForm/SignupForm' // import the SignupForm
 
 const App = () => {
   const [user, setUser] = useState(null)
@@ -18,6 +19,7 @@ const App = () => {
         ) : (
           <Route path="/" element={<Landing />} />
         )}
+        <Route path='/signup' element={<SignupForm setUser={setUser} />} />
       </Routes>
     </>
   )
